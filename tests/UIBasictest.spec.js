dcd63b348591102ec76648e573c927f18e2a4207
Szymon <wasiak.szymon.1991@gmail.com>
2022-11-20 14:59:02 +0100

    Add login page test case

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -1,12 +1,25 @@
 const {test, expect} = require('@playwright/test');
 
-test('Browser Context declaration Playwright test',async ({browser})=>
+test.only('Browser Context declaration Playwright test',async ({browser})=>
 {
-    
     const context =await browser.newContext();
     const page = await context.newPage();
-    await page.goto('https://rahulshettyacademy.com/loginpagePractice/');
+
+    const email = page.locator("#email");
+    const password = page.locator("#password");
+    const buttonSubmint = page.locator("[name=commit]");
+
+    await page.goto('https://sso.teachable.com/secure/9521/identity/login');
     console.log(await page.title())
+    await email.type("wasiak.szymon.1991@gmail.com");
+    await password.type("vvv");
+    await buttonSubmint.click();
+    console.log(await page.locator("[data-transition=entered]").textContent());
+    await expect(page.locator("[data-transition=entered]")).toContainText(' password is incorrect');
+    await email.fill("wasiak.szymon.1991@gmail.com");
+    await password.type("Hellscream#91");
+    await buttonSubmint.click();
+    console.log(await page.locator("body > div.view-school > div.view-directory.course-directory.signed-in-directory > div > div.row.course-list.list > div:nth-child(1) > div > div.row > a > div > div.course-listing-title").textContent());
 })
 
 test('First Playwright test',async ({page})=>
